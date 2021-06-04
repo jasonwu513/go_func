984a25cb723d354dc4da2842f739958316544947
jasonwu513 <jasonwu513@gmail.com>
2021-06-05 06:17:14 +0800

    go_func init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	result := StringToLower("AbcdefGH", processCase) 
+	fmt.Println(result)
+	result = StringToLower2("AbcdefGH", processCase)
+	fmt.Println(result)
+
+	func ProcessCase (str String) string {
+		result := ""
+		for i , value := range str (
+			
+		)
+	}
+
+}
